d7c1a01ffd8ec0b9050cd7cf7fef2de6486ea7d8
KonuTech <borowiec.k@gmail.com>
2024-02-03 21:59:21 +0000

    M01

---
+++ data-engineering-zoomcamp-gcp/temp/terraform/gcp/variables.tf
@@ -0,0 +1,76 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "/home/pd2669/data-engineering-zoomcamp-gcp/02-workflow-orchestration/big-bliss-411815-e70eba10e18f.json"
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+}
+
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+  default     = "mage-data-prep"
+}
+
+variable "container_cpu" {
+  description = "Container cpu"
+  default     = "2000m"
+}
+
+variable "container_memory" {
+  description = "Container memory"
+  default     = "2G"
+}
+
+variable "project_id" {
+  type        = string
+  description = "The name of the project"
+  default     = "big-bliss-411815"
+}
+
+variable "region" {
+  type        = string
+  description = "The default compute region"
+  default     = "europe-central2"
+}
+
+variable "zone" {
+  type        = string
+  description = "The default compute zone"
+  default     = "europe-central2-b"
+}
+
+variable "repository" {
+  type        = string
+  description = "The name of the Artifact Registry repository to be created"
+  default     = "mage-data-prep"
+}
+
+variable "database_user" {
+  type        = string
+  description = "The username of the Postgres database."
+  default     = "postgres"
+}
+
+variable "database_password" {
+  type        = string
+  description = "The password of the Postgres database."
+  sensitive   = true
+}
+
+variable "docker_image" {
+  type        = string
+  description = "The docker image to deploy to Cloud Run."
+  default     = "mageai/mageai:latest"
+}
+
+variable "domain" {
+  description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
+  type        = string
+  default     = ""
+}
+
+variable "ssl" {
+  description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
+  type        = bool
+  default     = false
+}
